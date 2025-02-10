bd1dbf46d6a396082d57aa21e0d29a5e58dbad71
NinProVidz <1304122@edu.burnabyschools.ca>
2025-02-10 10:22:50 -0800

    ye

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.CompareTag("Player"))
+            {
+                GetComponent<PlayableDirector>().Play();
+            }
+        }
+    }
+}//e
+
