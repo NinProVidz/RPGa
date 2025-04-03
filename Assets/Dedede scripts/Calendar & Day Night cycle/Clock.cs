076525d8c14521fcf7934a60359b1159b1ed5c8b
Draconic <1391149@edu.burnabyschools.ca>
2025-04-03 10:09:03 -0700

    DATE AND CLOCK BABY

--- Assets/Dedede scripts/Calendar & Day Night cycle/Clock.cs
+++ Assets/Dedede scripts/Calendar & Day Night cycle/Clock.cs
@@ -8,19 +8,36 @@ public class Clock : MonoBehaviour
     public event EventHandler<TimeSpan> ClockChange;
 
     [SerializeField] public float dayLength;
+   
+    [SerializeField] public int counterTillDayChange = 0;
+    [SerializeField] public int maxCounter = 2880;
+    [SerializeField] public int counterReset = 0;
 
-    private TimeSpan currentTime;
-    private float minuteLength => dayLength / ClockConstant.MinutesInDay;
+    Date date;
+
+    [SerializeField] public TimeSpan currentTime;
+    [SerializeField] public float minuteLength => dayLength / ClockConstant.MinutesInDay;
 
     private IEnumerator AddMinute()
     {
         currentTime += TimeSpan.FromMinutes(1);
         ClockChange?.Invoke(this, currentTime);
+        counterTillDayChange++;
         yield return new WaitForSeconds(minuteLength);
         StartCoroutine(AddMinute());
     }
     private void Start()
     {
+        date = FindObjectOfType<Date>();
         StartCoroutine(AddMinute());
+    }
+
+    private void Update()
+    {
+        if(counterTillDayChange == maxCounter)
+        {
+            date.day++;
+            counterTillDayChange = counterReset;
+        }
     }
 }
--- Assets/Dedede scripts/Calendar & Day Night cycle/Date.cs
+++ Assets/Dedede scripts/Calendar & Day Night cycle/Date.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Date : MonoBehaviour
 {
+
+    [SerializeField] public int month;
+    [SerializeField] public int day;
+
+    [SerializeField] TextMeshProUGUI dateText;
+
+    Clock clock;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        clock = FindObjectOfType<Clock>();
+        month = 7;
+        day = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        dateText.text = month.ToString() + "/" + day.ToString();
+        if(month == 7 && day == 31 && clock.counterTillDayChange == clock.maxCounter)
+        {
+            month++;
+            day = 0;
+        }
     }
 }
