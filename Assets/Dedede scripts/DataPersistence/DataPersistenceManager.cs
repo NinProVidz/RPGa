1f1dd82ab24a71124a3dae3a8e5b767dd7c8ea61
Draconic <1391149@edu.burnabyschools.ca>
2025-04-11 11:59:49 -0700

    ITS FIXED LETS GOOOOOOOOOOOO

--- Assets/Dedede scripts/DataPersistence/DataPersistenceManager.cs
+++ Assets/Dedede scripts/DataPersistence/DataPersistenceManager.cs
@@ -7,6 +7,8 @@ public class DataPersistenceManager : MonoBehaviour
 {
     private GameData gameData;
 
+    private List<IDataPersistence> dataPersistenceObjects;
+    
     public static DataPersistenceManager instance { get; private set; } //get the instance publicly but can only modify here
 
     private void Awake()
@@ -16,6 +18,11 @@ public class DataPersistenceManager : MonoBehaviour
             Debug.Log("THERES MORE THAN ONE INSTANCE OF THIS THING IN THE SCENE AAAAAAAAA");
         }
         instance = this;
+    }
+
+    private void Start()
+    {
+        this.dataPersistenceObjects = FindAllDataPersistenceObjects();
     }
 
     public void NewGame()
@@ -33,16 +40,35 @@ public class DataPersistenceManager : MonoBehaviour
             NewGame();
         }
         //push all loaded data to all scripts that need it
+        foreach(IDataPersistence dataPersistenceObj in dataPersistenceObjects)
+        {
+            dataPersistenceObj.LoadData(gameData);
+        }
     }
 
     public void SaveGame()
     {
         //push data to all scripts so they can be updated
+        foreach (IDataPersistence dataPersistenceObj in dataPersistenceObjects)
+        {
+            dataPersistenceObj.SaveData(ref gameData);
+        }
+
+        Debug.Log("Saved the date" + gameData.month + "/" + gameData.day);
+
         //save data to file using data handler
     }
 
     private void OnApplicationQuit()
     {
         SaveGame();
+    }
+
+    private List<IDataPersistence> FindAllDataPersistenceObjects()
+    {
+        IEnumerable<IDataPersistence> dataPersistenceObjects = FindObjectsOfType<MonoBehaviour>()
+            .OfType<IDataPersistence>();
+        return new List<IDataPersistence>(dataPersistenceObjects);
+
     }
 }
