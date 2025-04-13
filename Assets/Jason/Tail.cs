931904dab0fee1018561eb54c3521d36354e6b8d
FGDGHK <90111076+FGDGHK@users.noreply.github.com>
2025-04-12 22:26:05 -0700

    234234

---
+++ Assets/Jason/Tail.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class Tail : MonoBehaviour
+{
+    [Tooltip("Total links count")]
+    public int count = 10;
+    [Tooltip("Single link rotate time")]
+    public float delay = 0.1f;
+    [Tooltip("Tail look target")]
+    public Transform target;
+    [Tooltip("Tail links")]
+    public Transform[] links;
+    [Tooltip("Link forwards")]
+    public Vector3[] forwards;
+    [Tooltip("Link forward velocities")]
+    public Vector3[] velocities;
+
+    //private void OnValidate()
+    //{
+    //    if (!Application.isPlaying && gameObject.scene != null)
+    //        Invoke(nameof(Create), 0);
+    //}
+
+    private void Create()
+    {
+        while (transform.childCount > 0)
+            DestroyImmediate(transform.GetChild(0).gameObject);
+
+        if (target == null)
+        {
+            target = GameObject.CreatePrimitive(PrimitiveType.Cube).transform;
+            target.name = "Target";
+            target.parent = transform.parent;
+            target.localScale *= 0.5f;
+            target.localPosition = Vector3.forward;
+        }
+
+        links = new Transform[count];
+        var parent = transform;
+        for (int i = 0; i < count; i++)
+        {
+            links[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere).transform;
+            links[i].name = $"Link{i}";
+            links[i].parent = parent;
+            links[i].localScale = Vector3.one * (1f - 1f * i / count) / parent.lossyScale.x;
+            links[i].localPosition = Vector3.back / 2f;
+            parent = links[i];
+        }
+        links[0].localPosition = Vector3.zero;
+
+        forwards = new Vector3[count];
+        for (int i = 0; i < count; i++)
+            forwards[i] = links[i].forward;
+
+        velocities = new Vector3[count];
+    }
+
+    private void Update()
+    {
+        transform.LookAt(target);
+
+        for (int i = 0; i < count; i++)
+            links[i].forward = forwards[i] = Vector3.SmoothDamp(forwards[i], links[i].parent.forward, ref velocities[i], delay);
+    }
+}
